013d1dcee44899c174ec7c426d7f04b0ae755c62
Алексей Голубев <a.golubev@qsoft.ru>
2025-11-12 18:10:24 +0300

    инициализвация, немного начал изучение gin

---
+++ cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	todo "github.com/rest_api_gin"
+	"github.com/rest_api_gin/pkg/handler"
+	"github.com/rest_api_gin/pkg/repository"
+	"github.com/rest_api_gin/pkg/service"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("3000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
